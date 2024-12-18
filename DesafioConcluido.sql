46c84d42cd45b85b8e4c1d260b29cb75fcba4ca0
Erick Oliveira <ericksoliveira69@gmail.com>
2024-12-18 13:48:38 -0300

    Desafio Concluido

---
+++ DesafioConcluido.sql
@@ -0,0 +1,94 @@
+-- 1
+SELECT
+	Nome,
+	Ano
+FROM Filmes
+
+-- 2
+SELECT
+	Nome,
+	Ano,
+	Duracao
+FROM Filmes
+ORDER BY Ano
+
+-- 3
+SELECT
+	Nome,
+	Ano,
+	Duracao
+FROM Filmes
+WHERE Nome = 'De Volta para o Futuro'
+
+-- 4
+SELECT
+	Nome,
+	Ano,
+	Duracao
+FROM Filmes
+WHERE Ano = 1997
+
+-- 5
+SELECT
+	Nome,
+	Ano,
+	Duracao
+FROM Filmes
+WHERE Ano > 2000
+
+-- 6
+SELECT
+	Nome,
+	Ano,
+	Duracao
+FROM Filmes
+WHERE Duracao > 100 AND Duracao < 150
+ORDER BY Duracao
+
+-- 7 
+SELECT
+	Ano,
+	COUNT(*) Quantidade
+FROM Filmes
+GROUP BY Ano
+ORDER BY Quantidade DESC
+
+-- 8
+SELECT
+	*
+FROM Atores
+WHERE Genero = 'M'
+
+-- 9
+SELECT
+	*
+FROM Atores
+WHERE Genero = 'F'
+ORDER BY PrimeiroNome
+
+-- 10
+SELECT
+	Filmes.Nome,
+	Generos.Genero
+FROM FilmesGenero
+INNER JOIN Filmes ON FilmesGenero.IdFilme = Filmes.Id
+INNER JOIN Generos ON FilmesGenero.IdGenero = Generos.Id
+
+-- 11
+SELECT
+	Filmes.Nome,
+	Generos.Genero
+FROM FilmesGenero
+INNER JOIN Filmes ON FilmesGenero.IdFilme = Filmes.Id
+INNER JOIN Generos ON FilmesGenero.IdGenero = Generos.Id
+WHERE Genero = 'Mistério'
+
+-- 12
+SELECT
+	F.Nome,
+	A.PrimeiroNome,
+	A.UltimoNome,
+	Papel
+FROM ElencoFilme
+INNER JOIN Filmes F ON ElencoFilme.IdFilme = F.Id
+INNER JOIN Atores A ON ElencoFilme.IdAtor= A.Id
